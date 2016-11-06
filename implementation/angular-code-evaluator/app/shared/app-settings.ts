c323efa73b46c6235cd6bb1f62f30c0ba0f3d350
Manuel Zamith <mzamith10@gmail.com>
2016-11-06 18:36:37 +0000

    change url

--- implementation/angular-code-evaluator/app/shared/app-settings.ts
+++ implementation/angular-code-evaluator/app/shared/app-settings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-   public static get API_ENDPOINT(): string { return 'http://localhost:8080'; }
-   //public static get API_ENDPOINT(): string { return 'http://127.0.0.1:6666/api/'; }
+   //public static get API_ENDPOINT(): string { return 'http://localhost:8080'; }
+   public static get API_ENDPOINT(): string { return 'http:/192.168.58.62:8090'; }
 }
