9330e10831b7a9068a117d3136a9c5e1297fbdb7
Paulo Barbosa <1130648@isep.ipp.pt>
2016-11-13 17:10:58 +0000

    ed. submissionrepository

--- implementation/spring-code-evaluator/src/main/java/org/evaluator/ws/repository/SubmissionRepository.java
+++ implementation/spring-code-evaluator/src/main/java/org/evaluator/ws/repository/SubmissionRepository.java
@@ -1,8 +1,20 @@
 package org.evaluator.ws.repository;
 
+import java.util.Collection;
+
 import org.evaluator.ws.model.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+	
+	  /**
+     * Find Submissions By Exercise ID
+     * @param exerciseID Exercise ID
+     * @return Submissions
+     */
+    @Query("SELECT s FROM Submission s WHERE s.exercise.id = :exerciseID")
+    Collection<Submission> getSubmissionsByExercise(@Param("exerciseID") Long exerciseID);
 
 }
