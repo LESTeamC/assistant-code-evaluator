e3b3f76d8a1a283aeab5f075b205bf9f4cc2a727
Manuel Zamith <mzamith10@gmail.com>
2016-11-20 19:31:57 +0000

    comments

--- implementation/angular-code-evaluator/app/examiner/workstation/workstation.component.js
+++ implementation/angular-code-evaluator/app/examiner/workstation/workstation.component.js
@@ -24,8 +24,11 @@ var WorkstationComponent = (function () {
         this.activatedRoute = activatedRoute;
         this.examService = examService;
         this.authService = authService;
+        //DEMONSTRATION VARIABLE ONLY. DELETE FOR RELEASE
         this.testLongString = "\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et ligula venenatis, convallis odio ornare, posuere quam. Phasellus molestie leo purus, sit amet iaculis nunc gravida id. Curabitur pretium lacus eget consequat interdum. Vivamus augue leo, tempor sit amet gravida quis, fringilla in nisl. Aenean pretium blandit ligula. Integer dignissim, mauris vel consequat porttitor, arcu urna accumsan ante, ac blandit sapien mauris at magna. Praesent ultrices molestie viverra. Sed pellentesque tempor consectetur. Curabitur faucibus risus eget elit gravida hendrerit. Integer odio lectus, faucibus a bibendum eu, sollicitudin ut massa. Nam et eros eu justo hendrerit tincidunt viverra quis augue. Nullam ac rutrum diam. Ut nec sem dignissim ante tristique molestie et a lorem. Vivamus egestas quam vitae sollicitudin vehicula. Fusce elementum lacus et metus blandit, et pulvinar dui maximus. Mauris at ultrices turpis.\n\nProin auctor efficitur eros id vehicula. Vivamus porttitor porttitor nibh, a iaculis nunc gravida accumsan. Sed tristique justo purus, vitae posuere elit ultricies a. Praesent eros nunc, accumsan a tempus et, dapibus in orci. Quisque placerat sit amet orci sit amet maximus. Fusce rhoncus placerat massa sed consectetur. Sed semper bibendum libero et vulputate. Nunc pulvinar magna et justo iaculis aliquam. Praesent aliquet nibh tortor, vel scelerisque leo suscipit quis.\n\nIn hac habitasse platea dictumst. Morbi a bibendum arcu, ut porta diam. Etiam congue ante non blandit vulputate. Duis feugiat viverra felis vel consectetur. Ut neque est, sagittis a erat non, pretium convallis neque. Duis rhoncus neque leo, ac posuere erat blandit a. Nunc tempor ipsum eget lectus sollicitudin, in porta erat luctus. Quisque ac fringilla massa, ut egestas diam. Praesent ut erat odio.\n\nMorbi eleifend et felis nec maximus. Mauris et nisl viverra arcu vestibulum ultricies sed ac mauris. Quisque facilisis lorem a orci iaculis, quis vehicula urna tempor. Phasellus sapien nunc, efficitur eu tincidunt id, elementum vitae magna. Vivamus in elementum metus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc eget faucibus ipsum. Nulla elementum risus sapien, ac tempor est varius ac. Duis sodales, mauris at accumsan varius, lorem purus aliquam mi, a tempor elit erat quis nisl. Pellentesque tempor sollicitudin risus, non condimentum ex ullamcorper et. Vivamus dignissim consectetur elit, in iaculis nulla fermentum vitae. Maecenas dictum eu purus at tincidunt. Integer posuere elit erat, ac finibus libero tristique nec. Morbi id viverra nulla.\n\nSuspendisse velit massa, fringilla id lorem et, feugiat blandit arcu. Morbi nulla diam, cursus eget dui vitae, condimentum tempus quam. Cras placerat dapibus lorem, eget porta sem varius quis. Curabitur at vestibulum magna. Sed scelerisque elit semper magna iaculis varius. Curabitur pulvinar eros odio, efficitur bibendum risus iaculis non. Sed interdum enim pellentesque laoreet consectetur. Nam sed consequat neque. Maecenas lectus metus, condimentum pharetra mauris in, dapibus mollis enim. Suspendisse nec nulla porttitor, ornare mauris in, lobortis libero. Pellentesque rhoncus, quam ac venenatis fringilla, urna mi tristique dui, sit amet ultricies leo mauris posuere eros. Vestibulum id erat augue. Fusce quis quam quis mi commodo scelerisque. Morbi a massa sit amet nunc viverra fringilla sit amet eget risus.\n    ";
+        //DEMONSTRATION VARIABLE ONLY DELETE FOR RELEASE
         this.testLongCode = "\n/*\n * C Program To Identify the Missing Number in an Integer \n * Array of Size N-1 with Numbers[1,N]\n */\n#include <stdio.h>\n#define MAX 15\nint missing_number_array(int [],int);\n \nint main()\n{\n    int a[MAX], num, i, n;\n \n    printf(\"enter the range of array&bsol;n\");\n    scanf(\"%d\", &n);\n    for (i = 0;i < n;i++)\n    {\n        printf(\"enter a[%d]element into the array:\", i);\n        scanf(\"%d\", &a[i]);\n    }\n    num = missing_number_array(a, n);\n    printf(\"The missing number -> %d\n\", num);\n}\n \n/* To find the missing number in array */\nint missing_number_array(int a[],  int n)\n{\n    int i;\n    int s1 = 0; \n    int s2 = 0; \n \n    for (i = 0;i < n;i++)\n        s1 = s1 ^ a[i];\n    for (i = 1;i <= n + 1;i++)\n        s2 = s2 ^ i; \n    return (s1 ^ s2);\n}\n    ";
+        //relevant variables for binding in DOM
         this.submission = new submission_1.Submission();
         this.student = new student_1.Student();
         this.exercise = new exercise_1.Exercise();
@@ -34,10 +37,14 @@ var WorkstationComponent = (function () {
         this.comment = "";
         this.criteria = new Array();
         this.codeString = "";
+        //error and success variables to show alert messages
         this.gradeSuccess_ = false;
         this.successComment = false;
         this.gradeError = false;
     }
+    /**
+     * Init: Get submission and Exam content from API
+     */
     WorkstationComponent.prototype.ngOnInit = function () {
         var _this = this;
         this.activatedRoute.params
@@ -47,6 +54,11 @@ var WorkstationComponent = (function () {
     WorkstationComponent.prototype.isSubmitted = function () {
         return (this.submission.status === "C");
     };
+    /**
+     * Success Funtion for call of Submission data
+     * If successfull, get exam data!
+     * @param: data: submission object
+     */
     WorkstationComponent.prototype.success = function (data) {
         this.submission = data;
         this.criteria = data.criteria;
@@ -58,9 +70,17 @@ var WorkstationComponent = (function () {
         hljs.highlightBlock(this.codeElement.nativeElement);
         this.getExam(data.id);
     };
+    /**
+     * Fail Funtion for call of Submission data
+     * Navigates to dashboard
+     */
     WorkstationComponent.prototype.fail = function (error) {
         this._router.navigate(['/examiner/dashboard']);
     };
+    /**
+     * Function to get Exam data from API
+     * @param: id - Submission ID
+     */
     WorkstationComponent.prototype.getExam = function (id) {
         var _this = this;
         this.examService.getExamBySubmission(id)
@@ -73,9 +93,13 @@ var WorkstationComponent = (function () {
     WorkstationComponent.prototype.examFail = function (error) {
         this.fail(error);
     };
+    /**
+     * Save evaluation in database.
+     * When button is clicked
+     */
     WorkstationComponent.prototype.saveEvaluation = function () {
-        //console.log(this.criteria);
         var _this = this;
+        //Set variables in main submission object
         this.submission.comment = this.comment;
         this.submission.exercise = this.exercise;
         this.submission.criteria = this.criteria;
@@ -95,6 +119,10 @@ var WorkstationComponent = (function () {
         this.gradeSuccess_ = false;
         this.gradeError = true;
     };
+    /**
+     * Save comment in Db
+     * When button is clicked
+     */
     WorkstationComponent.prototype.saveComment = function () {
         var _this = this;
         this.submissionService.insertComment(this.submission.id, this.comment)
@@ -106,17 +134,34 @@ var WorkstationComponent = (function () {
     WorkstationComponent.prototype.commentFail = function (error) {
         this.successComment = false;
     };
+    /**
+     * Auxiliary function to calculate grade in percentage
+     * @param num: grade number
+     * @param gama: maximum grade
+     * @returns percentage value for grade
+     */
     WorkstationComponent.prototype.calcGrade = function (num, gama) {
         return num * 100 / gama;
     };
+    /**
+     * Auxiliary function to grade from list of criteria
+     * @param subCriteria- Array of criteria
+     * @returns percentage value for total grade given weights
+     */
     WorkstationComponent.prototype.calcTotalGrade = function (subCriteria) {
         var grade = 0;
         for (var _i = 0, subCriteria_1 = subCriteria; _i < subCriteria_1.length; _i++) {
             var s = subCriteria_1[_i];
+            //ignore this error
             grade += (parseInt(s.grade) * s.criteria.weight * 0.01);
         }
         return (grade < 0) ? 0 : grade;
     };
+    /**
+     * Auxiliary function to grade from list of criteria
+     * @param subCriteria- Array of criteria
+     * @returns percentage value for total grade given weights
+     */
     WorkstationComponent.prototype.existsUngraded = function () {
         var ungraded = false;
         for (var _i = 0, _a = this.criteria; _i < _a.length; _i++) {
@@ -126,6 +171,11 @@ var WorkstationComponent = (function () {
         }
         return false;
     };
+    /**
+     * Auxiliary function to create an array os consecutive integers
+     * @param num - maximum number in the array
+     * @returns array of consecutive integers , like [0, 1, 2]
+     */
     WorkstationComponent.prototype.createArray = function (num) {
         var array = Array.from(Array(num + 1), function (x, i) { return i; });
         return array;
@@ -142,6 +192,9 @@ var WorkstationComponent = (function () {
             this._router.navigate(['/examiner/dashboard']);
         }
     };
+    /**
+     * Auxiliary function to help determine witch option to select
+     */
     WorkstationComponent.prototype.select = function (num, grade) {
         return num === parseInt(grade);
     };
--- implementation/angular-code-evaluator/app/examiner/workstation/workstation.component.ts
+++ implementation/angular-code-evaluator/app/examiner/workstation/workstation.component.ts
@@ -24,6 +24,7 @@ declare var hljs: any;
 })
 export	class	WorkstationComponent implements OnInit	{
 
+//DEMONSTRATION VARIABLE ONLY. DELETE FOR RELEASE
     private testLongString:string = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et ligula venenatis, convallis odio ornare, posuere quam. Phasellus molestie leo purus, sit amet iaculis nunc gravida id. Curabitur pretium lacus eget consequat interdum. Vivamus augue leo, tempor sit amet gravida quis, fringilla in nisl. Aenean pretium blandit ligula. Integer dignissim, mauris vel consequat porttitor, arcu urna accumsan ante, ac blandit sapien mauris at magna. Praesent ultrices molestie viverra. Sed pellentesque tempor consectetur. Curabitur faucibus risus eget elit gravida hendrerit. Integer odio lectus, faucibus a bibendum eu, sollicitudin ut massa. Nam et eros eu justo hendrerit tincidunt viverra quis augue. Nullam ac rutrum diam. Ut nec sem dignissim ante tristique molestie et a lorem. Vivamus egestas quam vitae sollicitudin vehicula. Fusce elementum lacus et metus blandit, et pulvinar dui maximus. Mauris at ultrices turpis.
 
@@ -35,7 +36,7 @@ Morbi eleifend et felis nec maximus. Mauris et nisl viverra arcu vestibulum ultr
 
 Suspendisse velit massa, fringilla id lorem et, feugiat blandit arcu. Morbi nulla diam, cursus eget dui vitae, condimentum tempus quam. Cras placerat dapibus lorem, eget porta sem varius quis. Curabitur at vestibulum magna. Sed scelerisque elit semper magna iaculis varius. Curabitur pulvinar eros odio, efficitur bibendum risus iaculis non. Sed interdum enim pellentesque laoreet consectetur. Nam sed consequat neque. Maecenas lectus metus, condimentum pharetra mauris in, dapibus mollis enim. Suspendisse nec nulla porttitor, ornare mauris in, lobortis libero. Pellentesque rhoncus, quam ac venenatis fringilla, urna mi tristique dui, sit amet ultricies leo mauris posuere eros. Vestibulum id erat augue. Fusce quis quam quis mi commodo scelerisque. Morbi a massa sit amet nunc viverra fringilla sit amet eget risus.
     `
-
+//DEMONSTRATION VARIABLE ONLY DELETE FOR RELEASE
     private testLongCode:string = `
 /*
  * C Program To Identify the Missing Number in an Integer 
@@ -75,7 +76,7 @@ int missing_number_array(int a[],  int n)
 }
     `
 
-
+    //relevant variables for binding in DOM
     private submission = new Submission();
     private student = new Student();
     private exercise = new Exercise();
@@ -87,6 +88,7 @@ int missing_number_array(int a[],  int n)
     private criteria = new Array<SubmissionCriteria>();
     private codeString:string = "";
 
+    //error and success variables to show alert messages
     private gradeSuccess_:boolean = false;
     private successComment:boolean = false;
     private gradeError:boolean = false;
@@ -97,6 +99,9 @@ int missing_number_array(int a[],  int n)
     
     @ViewChild('code') codeElement: ElementRef;
 
+    /**
+     * Init: Get submission and Exam content from API
+     */
     ngOnInit(){
         this.activatedRoute.params
             // (+) converts string 'id' to a number
@@ -106,11 +111,15 @@ int missing_number_array(int a[],  int n)
 
     }
 
-    isSubmitted():boolean{
+    private isSubmitted():boolean{
         return (this.submission.status === "C");
     }
 
-
+    /**
+     * Success Funtion for call of Submission data
+     * If successfull, get exam data!
+     * @param: data: submission object
+     */
     success(data:any){
         this.submission = data;
         this.criteria = data.criteria;
@@ -126,10 +135,18 @@ int missing_number_array(int a[],  int n)
         this.getExam(data.id);
     }
 
+    /**
+     * Fail Funtion for call of Submission data
+     * Navigates to dashboard
+     */
     fail(error:any){
         this._router.navigate(['/examiner/dashboard']);
     }
 
+    /**
+     * Function to get Exam data from API
+     * @param: id - Submission ID
+     */
     getExam(id:number){
         this.examService.getExamBySubmission(id)
             .subscribe(data => this.examSuccess(data),
@@ -145,9 +162,14 @@ int missing_number_array(int a[],  int n)
         this.fail(error)
     }
 
-    saveEvaluation(){
-        //console.log(this.criteria);
 
+    /**
+     * Save evaluation in database.
+     * When button is clicked
+     */
+    saveEvaluation(){
+
+        //Set variables in main submission object
         this.submission.comment = this.comment;
         this.submission.exercise = this.exercise;
         this.submission.criteria = this.criteria;
@@ -175,6 +197,10 @@ int missing_number_array(int a[],  int n)
         this.gradeError = true;
     }
 
+    /**
+     * Save comment in Db
+     * When button is clicked
+     */
     saveComment(){
 
          this.submissionService.insertComment(this.submission.id, this.comment)
@@ -190,14 +216,27 @@ int missing_number_array(int a[],  int n)
         this.successComment = false;
     }
 
-    calcGrade(num:any, gama:any):number{
+    /**
+     * Auxiliary function to calculate grade in percentage
+     * @param num: grade number
+     * @param gama: maximum grade
+     * @returns percentage value for grade
+     */
+    private calcGrade(num:any, gama:any):number{
         return num * 100 / gama;
     }
 
 
-    calcTotalGrade(subCriteria:SubmissionCriteria[]):number{
+    /**
+     * Auxiliary function to grade from list of criteria
+     * @param subCriteria- Array of criteria
+     * @returns percentage value for total grade given weights
+     */
+    private calcTotalGrade(subCriteria:SubmissionCriteria[]):number{
         var grade:number = 0;
         for (let s of subCriteria){
+
+            //ignore this error
             grade += (parseInt(s.grade) * s.criteria.weight * 0.01);
         }
 
@@ -205,7 +244,12 @@ int missing_number_array(int a[],  int n)
 
     }
 
-    existsUngraded():boolean{
+    /**
+     * Auxiliary function to grade from list of criteria
+     * @param subCriteria- Array of criteria
+     * @returns percentage value for total grade given weights
+     */
+    private existsUngraded():boolean{
         var ungraded:boolean = false;
         for (let s of this.criteria){
             if ((s.grade) < 0) return true;
@@ -215,6 +259,11 @@ int missing_number_array(int a[],  int n)
 
     }
 
+    /**
+     * Auxiliary function to create an array os consecutive integers
+     * @param num - maximum number in the array
+     * @returns array of consecutive integers , like [0, 1, 2]
+     */
     private createArray(num:number):number[]{
 
         var array = Array.from(Array(num + 1),(x,i)=>i);
@@ -235,8 +284,9 @@ int missing_number_array(int a[],  int n)
         }
     }
 
-
-
+    /**
+     * Auxiliary function to help determine witch option to select
+     */
     select(num:number, grade:any){
         return num === parseInt(grade);
     }
