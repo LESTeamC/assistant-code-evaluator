563215cceb9c4ef8dd0e539f5b089e9c6bfe9491
TheIAceI <rcaldas46@gmail.com>
2016-11-29 15:28:39 +0000

    Update view-examiners.component.ts
    There are a few functions heres that are not beeing used right now. Its for future usage like delete examiner.

--- implementation/angular-code-evaluator/app/admin/view-examiners/view-examiners.component.ts
+++ implementation/angular-code-evaluator/app/admin/view-examiners/view-examiners.component.ts
@@ -1,15 +1,78 @@
-import {Component, OnInit}	from '@angular/core';
+import {Component, OnInit, ViewChild}	from '@angular/core';
 import {Router} from '@angular/router';
+import { Examiner } from './../../model/examiner'
+import { ExaminerService } from './../../shared/examiner.service'
+import { Account } from './../../model/account';
+import { Observable } from 'rxjs/Rx';
+import { ModalDirective } from 'ng2-bootstrap/ng2-bootstrap';
+
 
 @Component({	
     selector: 'admin',	
-	template: `<h1>VIEW EXAMINERS</h1>`,
+	  templateUrl: `app/admin/view-examiners/view-examiners.component.html`,
+    styleUrls: ['app/admin/view-examiners/view-examiners.component.css']
 })
-export	class	ViewExaminersComponent implements OnInit	{
 
-    constructor(private _router:Router){}
+
+
+export	class	ViewExaminersComponent implements OnInit	{
+    
+    private examiners:Examiner[];
+    private selectedExaminerModal:Examiner = null;
+    private selectedExaminer:Examiner = new Examiner();
+
+    messageDelete='';
+
+    constructor(private _router: Router, private examinerService:ExaminerService) { }
 
     ngOnInit(){
-        console.log("view examiners");
+        this.examinerService.getExaminers()
+            .subscribe(data => this.successGetExaminers(data),
+                       error => this.failGetExaminers(error));
     }
+
+
+
+    private isSelected(id:number):boolean{
+        return id === this.selectedExaminer.id;
+    }
+
+    private selectRow(examiner:Examiner):void{
+        this.selectedExaminer = examiner;
+    }
+
+    successGetExaminers(data:any){
+            this.examiners = data;
+    }
+
+    failGetExaminers(error:any){
+            this._router.navigate(['/loginadmin']);
+    }
+
+
+
+    deleteExaminer(){
+        this.hideChildModal();
+
+    }
+
+
+   @ViewChild('deleteModal') public childModal:ModalDirective;
+ 
+     /**
+     * Shows modal
+     */
+
+    public showChildModal():void {
+        this.childModal.show();
+    }
+    
+    /**
+     * Hides modal
+     */
+    public hideChildModal():void {
+        this.childModal.hide();
+    }
+
+
 }	
