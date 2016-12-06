9cca03d86c160e6efe2cdc512fde9e696ac7a086
TheIAceI <rcaldas46@gmail.com>
2016-12-06 23:52:04 +0000

    Update SendEmail.java

--- implementation/spring-code-evaluator/src/main/java/org/evaluator/ws/web/api/SendEmail.java
+++ implementation/spring-code-evaluator/src/main/java/org/evaluator/ws/web/api/SendEmail.java
@@ -1,3 +1,17 @@
+package org.evaluator.ws.web.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Created by Ricardo on 05/12/2016.
+ */
+
 @Component
 public class SendEmail {
 
